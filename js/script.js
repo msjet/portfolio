45764c755534b533bbce9ae3a46cbfd756308949
DESKTOP-TJ6CLEF\pc07 <msato20161128@gmail.com>
2017-04-20 14:18:21 +0900

    変更

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(function(){
+   $(window).scroll(function () {
+        var $scrolltop = $(window).scrollTop();
+        if ($scrolltop < 500) {
+            $('#pagetop').hide();
+        } else {
+            $('#pagetop').show();
+        }
 
+    });
+    
+    $('#pagetop').click(function(){
+        $('html,body').animate({
+            scrollTop:0
+        },1000);
+        return false;
+    });
+    
 })
