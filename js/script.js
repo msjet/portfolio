e227cc26a500bbbdf97e3ae26e65ea1e8551398d
DESKTOP-TJ6CLEF\pc07 <msato20161128@gmail.com>
2017-04-28 14:44:05 +0900

    変更しました

--- js/script.js
+++ js/script.js
@@ -24,4 +24,15 @@ $(function () {
         $(this).toggleClass('active');
         return false;
     });
+    
+    $('#gnav a').click(function () {
+        //        $(this).attr('href');
+        var target = $($(this).attr('href')).offset().top;
+        target -= 0;
+        $('html,body').animate({
+            scrollTop: target
+        }, 500);
+
+        return false;
+    });
 });
